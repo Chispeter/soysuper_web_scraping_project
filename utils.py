7d26087e3fba5a268ac564af0992926ab8ea5b57
Chispeter <pnavgar2805@gmail.com>
2024-01-19 18:37:00 +0100

    Change replace chars in get_cleaned_str()

--- utils.py
+++ utils.py
@@ -42,9 +42,9 @@ def write_json(data_input: any, current_dir: str, data_dirname: str, indent: int
     file.close()
     
 
-## Esta función se utiliza para limpiar un string y eliminar caracteres insignificantes
+## Esta función se utiliza para limpiar un string y eliminar caracteres que generan problemas
 def get_cleaned_str(str_input: str) -> str:
-    return str_input.lower().replace(" / ", "_").replace(" - ", "_").replace(" ", "_").replace(",", "")
+    return str_input.replace(" \ ", " - ").replace(" / ", " - ")
 
 
 ## Esta función se utiliza para comprobar si ya se habían extraído los datos de los productos anteriormente
